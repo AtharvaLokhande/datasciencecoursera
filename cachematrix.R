cbcb4f2430801a9718f66c2c5f837547649c5bb5
AtharvaLokhande <64296752+AtharvaLokhande@users.noreply.github.com>
2020-06-17 14:22:21 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-  invrs <- NULL   
-  set <- function(y) {
-    x <- y          #function for 
-    invrs <- NULL
+makeCacheMatrix <- function(x = matrix()) { 
+  inv <- NULL                             
+  set <- function(y) {                    ## define the set function to assign new 
+    x <<- y                             ## value of matrix in parent environment
+    inv <<- NULL                        ## if there is a new matrix, reset inv to NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) invrs <- inverse
-  getinverse <- function() invrs
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+  get <- function() x                     ## define the get fucntion - returns value of the matrix argument
+  
+  setinverse <- function(inverse) inv <<- inverse  ## assigns value of inv in parent environment
+  getinverse <- function() inv                     ## gets the value of inv where called
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
+  ## to the functions with the $ operator
 }
 
 
 ## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve will retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-  invrs <= x$getinverse()
-  if(!is.null(invrs)) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
     message("getting cached data")
-    return(invrs)
+    return(inv)
   }
-  mat <- x$get()
-  invrs <- solve(mat, ...)
-  x$setinverse(invrs)
-  invrs
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
